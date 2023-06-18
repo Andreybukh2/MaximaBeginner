994004fd27f19e5ff713b8ce865f1247f9a21c0e
Andreybukh2 <121665325+Andreybukh2@users.noreply.github.com>
2023-06-19 00:55:38 +0300

    HW03 Операторы

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import static org.example.Homework03.*;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        System.out.print("HW03-Решение задачи №1 - "); CompareNumber(3);
+        System.out.print("HW03-Решение задачи №2 - "); IntervalNumber(150);
+        System.out.print("HW03-Решение задачи №3 - "); Seasons(10);
+        System.out.print("HW03-Решение задачи №4 - "); Calculation(10);
+        System.out.print("HW03-Решение задачи №5 - "); LeapYear(1600);
+        System.out.print("HW03-Решение задачи №6 - "); Triangle(5.5,4,3);
+        System.out.print("HW03-Решение задачи №7 - "); Trafficlight(2.5);
     }
 }
